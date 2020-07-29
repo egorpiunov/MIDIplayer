7476f2e96fc33585850beb6d13a9cd1084babab6
egorpiunov <egor.piunov64@gmail.com>
2020-07-29 11:48:56 +0300

    я обосрался

--- player.py
+++ player.py
@@ -1,2 +1,9 @@
-a = input('Enter your name:\n')
-print(f'Hello {a}!')
+from miditime.miditime import MIDITime
+mymidi = MIDITime(120, 'myfile.mid')
+midinotes = [
+    [0, 60, 127, 3],
+    [10, 61, 127, 4],
+    [20, 63, 120, 5]
+]
+mymidi.add_track(midinotes)
+mymidi.save_midi()
