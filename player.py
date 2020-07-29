fa8fe51f3dd13901b7d32df994ad623961eb29c1
Anton <piunov.doc@yandex.ru>
2020-07-29 12:16:27 +0300

    C major gamma

--- player.py
+++ player.py
@@ -1,9 +1,22 @@
 from miditime.miditime import MIDITime
 mymidi = MIDITime(120, 'music.mid')
-midinotes = [
-    [0, 60, 127, 3],
-    [10, 61, 127, 4],
-    [20, 63, 120, 5]
-]
+notes = ['C', 'D', 'E', 'F', 'G', 'A', 'B', 'C+1']
+pitches = {
+    'C' : 60,
+    'D' : 62,
+    'E' : 64,
+    'F' : 65,
+    'G' : 67,
+    'A' : 69,
+    'B' : 71,
+    'C+1' : 72
+}
+midinotes = []
+time = 0
+i = 0
+for note in notes:
+    midinotes.append([time, pitches[notes[i]], 127, 2])
+    i += 1
+    time += 2
 mymidi.add_track(midinotes)
 mymidi.save_midi()
